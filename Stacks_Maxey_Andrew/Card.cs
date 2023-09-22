f3ccd0bcf2b7ef826cf0dba2d184c18fbe450de8
KillaGoose51 <93846546+KillaGoose51@users.noreply.github.com>
2023-09-22 14:15:40 -0500

    Initial commit

---
+++ Stacks_Maxey_Andrew/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_Maxey_Andrew
+{
+    internal class Card
+    {
+    
+        public string Number { get; }
+        public string Suit { get; }
+        // get the secounds word in the sentence and assigns it to Suit
+
+        // Constructor assigns values to be used in a "New" object
+        public Card(string numParam, string suitParam)
+        {//Card("A", "Spades") turns the Number into a Number Param so now A = numParam
+            //param resets the variable allowing new ones to be put through making a new object
+            Number = numParam;
+            //Param = placeholder, param allows change 
+            Suit = suitParam;
+        }
+
+    }
+}
---
+++ Stacks_Maxey_Andrew/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks_Maxey_Andrew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Card[] myCardsArray = new Card[]
+            {
+                new Card("Ace", "Spades"),
+                 // get the first word in the sentence and assigns it to Number so for this one it will be "A"
+                new Card("Jack", "Clubs"),
+                new Card("Q", "Diamonds"),
+                new Card("Q", "Hearts"),
+                new Card("10", "Hearts"),
+                new Card("2", "Diamonds"),
+                new Card("6", "Spades"),
+                new Card("9", "Clubs"),
+                new Card("A", "Diamonds"),
+            };
+
+            Stack<Card> startingDeck = new Stack<Card>(myCardsArray);
+
+            Console.WriteLine("Cards in Deck:");
+            foreach(Card card in startingDeck)
+            {// so now you are calling the card object with the number value being called and because A is now reffered to as number it will print A
+                Console.WriteLine($"The {card.Number} of {card.Suit}!");
+            }
+
+            Console.WriteLine($"Cards in the deck: {startingDeck.Count}");
+
+            List<Card> myHand = new List<Card>();
+            myHand.Add(new Card("Jack", "Diamonds"));
+            myHand.Add(new Card("8", "Hearts"));
+            myHand.Add(new Card("3", "Diamonds"));
+
+            Console.WriteLine("Got any 8's?");
+            if(startingDeck.Count > 0)
+            {
+                myHand.Add(startingDeck.Pop());
+            }
+
+            Console.WriteLine($"Cards in the stack: {startingDeck.Count}");
+
+            startingDeck.Push(myHand[0]);
+            myHand.RemoveAt(0);
+
+            Console.WriteLine("Cards in Deck:");
+            foreach (Card card in startingDeck)
+            {
+                Console.WriteLine($"The {card.Number} of {card.Suit}!");
+            }
+
+        }
+    }
+}
